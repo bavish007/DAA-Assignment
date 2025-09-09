92f11e7c273c5aebc3d48dbc2d09ecd7135c623c
Bavish Reddy Muske <181584605+bavish007@users.noreply.github.com>
2025-09-09 22:45:41 +0530

    Create Union-by_Rank.py

---
+++ Sets-Weight_Methods/Union-by_Rank.py
@@ -0,0 +1,22 @@
+class DSURank:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.rank[ra] < self.rank[rb]:
+            self.parent[ra] = rb
+        elif self.rank[ra] > self.rank[rb]:
+            self.parent[rb] = ra
+        else:
+            self.parent[rb] = ra
+            self.rank[ra] += 1
+        return True
